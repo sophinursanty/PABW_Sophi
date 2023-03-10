5b48be67389b484931d80a403c5eb3657241dcc8
sophie <sophinursanty@studentelkomuniversity.ac.id>
2023-03-10 10:46:55 +0700

    commit V1.0

--- looping.py
+++ looping.py
@@ -1,11 +1,24 @@
-def is_even(number):
-    if number % 2 == 0:
-        return True
-    else:
-        return False
+arsene_lupin = {
+    'nama': 'Arsene Lupin',
+    'nik': 1234,
+    'jenis_kelamin': 'male',
+    'tanggal_lahir': '1902-03-23'
+}
 
-for i in range(1, 101):
-    if is_even(i) == False:
-        print(i)
-    else:
-        continue
+sherlock_holmes = {
+    'nama': 'Sherlock Holmes',
+    'nik': 4321,
+    'jenis_kelamin': 'male',
+    'tanggal_lahir': '1876-08-16'
+}
+
+irene_adler = {
+    'nama': 'Irene Adler',
+    'nik': 6789,
+    'jenis_kelamin': 'female',
+    'tanggal_lahir': '1884-10-07'
+}
+
+orang = [arsene_lupin, sherlock_holmes, irene_adler]
+
+print(orang)
